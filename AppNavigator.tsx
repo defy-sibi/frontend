cf53eabc271a61f23fb72cd14c1b8b106e4d055a
Sibi <sibi@Sibis-MacBook-Air.local>
2024-02-23 08:19:34 +0530

    project org. changes. Removed app,appnavigator duplicates

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -4,13 +4,15 @@ import Onboarding from './components/Onboarding';
 import {enableScreens} from 'react-native-screens';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import TabNavigator from './TabNavigator';
-
+import Home from './components/Home';
+import SubscriptionForm from './components/SubscriptionForm';
 enableScreens();
 
 // Define a type for the root stack param list
-type RootStackParamList = {
+export type RootStackParamList = {
   Onboarding: undefined;
   Home: undefined;
+  SubscriptionForm: undefined;
 };
 
 // Create a Stack Navigator
@@ -38,6 +40,7 @@ const AppNavigator = () => (
       />
       {/* Main is your new screen component */}
     </Stack.Navigator>
+    <Stack.Screen name="SubscriptionForm" component={SubscriptionForm} />
   </SafeAreaProvider>
 );
 
